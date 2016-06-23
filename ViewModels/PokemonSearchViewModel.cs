c68953aa12594de921461aa45cfff215d543ad88
Antonio Feregrino <antonio.feregrino@gmail.com>
2016-06-22 23:01:31 -0500

    Implementing INotifyPropertyChanged
    This will allow our views to be notified when a property is updated.

---
+++ ViewModels/PokemonSearchViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Mvvmdex.ViewModels
+{
+	public class PokemonSearchViewModel : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public PokemonSearchViewModel()
+		{
+		}
+
+	}
+}
+
