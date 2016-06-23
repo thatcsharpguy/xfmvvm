784118878fceb412b0248ef9c6e4d9e40ffd9a5f
Antonio Feregrino <antonio.feregrino@gmail.com>
2016-06-22 21:10:52 -0500

    Model... 👍

--- Models/Properties/MvvmdexClient.cs
+++ Models/Properties/MvvmdexClient.cs
@@ -1,11 +1,39 @@
-﻿using System;
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Jirapi;
+
 namespace Mvvmdex.Models
 {
 	public class MvvmdexClient
 	{
-		//PokeClient _client:
+		PokeClient _client;
 		public MvvmdexClient()
 		{
+			_client = new PokeClient();
+		}
+
+		public async Task<Pokemon> SearchForPokemon(string pokemonName)
+		{
+			// This could be anything, from a database to a connection to a web service:
+			try
+			{
+				var pkmn = await _client.Get<Jirapi.Resources.Pokemon>(pokemonName);
+				var species = await pkmn.Species.GetResource(_client);
+
+				return new Pokemon
+				{
+					Id = pkmn.Id,
+					Name = pkmn.Name,
+					Description = species.FlavorTextEntries
+					                     .FirstOrDefault(te => te.Language.Name == "en")?
+					                     .FlavorText.Replace("\n",""),
+					Shape = species.Shape.Name
+				};
+			}
+			catch
+			{
+				return null;
+			}
 		}
 	}
 }
