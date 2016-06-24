33c5e7d3463bee7c330fed109fb4ce7bcc8f4603
Antonio Feregrino <antonio.feregrino@gmail.com>
2016-06-24 15:10:25 -0500

    Command.

---
+++ ViewModels/BuscaPokemonCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Mvvmdex.ViewModels
+{
+    public class BuscaPokemonCommand : ICommand
+    {
+        private readonly Action<string> _search;
+        public BuscaPokemonCommand(Action<string> search)
+        {
+            _search = search;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (parameter != null)
+            {
+                var pokemon = (string) parameter;
+                return !String.IsNullOrEmpty(pokemon);
+            }
+            return false;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (CanExecute(parameter))
+            {
+                _search(parameter as string);
+            }
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
