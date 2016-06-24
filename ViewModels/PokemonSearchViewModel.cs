89bbeaaf164c63a9343b1cb63dec6a57bf454101
Antonio Feregrino <antonio.feregrino@gmail.com>
2016-06-24 15:10:06 -0500

    Raise property change event.

--- ViewModels/PokemonSearchViewModel.cs
+++ ViewModels/PokemonSearchViewModel.cs
@@ -1,16 +1,23 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Mvvmdex.Models;
 
 namespace Mvvmdex.ViewModels
 {
 	public class PokemonSearchViewModel : INotifyPropertyChanged
 	{
-		public event PropertyChangedEventHandler PropertyChanged;
 
-		public PokemonSearchViewModel()
-		{
-		}
+        public event PropertyChangedEventHandler PropertyChanged;
 
+	    public void RaiseOnPropertyChange([CallerMemberName] string propertyName = null)
+	    {
+	        if (PropertyChanged != null)
+	        {
+	            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+	        }
+	    }
 	}
 }
 
